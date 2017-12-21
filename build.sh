1f6a886e65aa0432b6393c3a8d0064b747389ade
Kuat Yessenov <kuat@google.com>
2017-12-20 16:38:44 -0800

    move controller main
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o pipefail
 set -x
 
 echo Building controller
-CGO_ENABLED=0 GOOS=linux go build -i -o controller-linux github.com/kyessenov/envoymesh/main
+CGO_ENABLED=0 GOOS=linux go build -i -o controller-linux github.com/kyessenov/envoymesh/cmd/controller
 
 echo Building container
 docker build -t gcr.io/istio-testing/envoymesh:latest .
